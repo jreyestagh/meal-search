681352b758c729a776b48b6e6fc31894032d897e
Joshua Reyestan <jreyesta@stu.southtexascollege.edu>
2024-12-14 22:37:17 -0600

    Meals By Ingredients Done

--- src/store/mutations.js
+++ src/store/mutations.js
@@ -1,4 +1,4 @@
-export function setSearchedMeals (state, meals) {
+export function setSearchedMeals(state, meals) {
     state.searchedMeals = meals || []
 }
 
@@ -6,6 +6,6 @@ export function setMealsByLetter(state, meals) {
     state.mealsByLetter = meals || []
 }
 
-export function setMealsByIngredients (state, meals) {
+export function setMealsByIngredient(state, meals) {
     state.mealsByIngredient = meals || []
 }
