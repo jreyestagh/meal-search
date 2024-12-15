e26f4c4867086b1974719e0de8d9f0f33a40288d
Joshua Reyestan <jreyesta@stu.southtexascollege.edu>
2024-12-14 21:53:20 -0600

    search by name is complete (and fixed MealItem.vue square corners)

--- src/components/MealItem.vue
+++ src/components/MealItem.vue
@@ -8,7 +8,7 @@ const { meal } = defineProps({
 </script>
 
 <template>
-    <div class="bg-white shadow rounded-x1">
+    <div class="bg-white shadow rounded-xl">
         <router-link :to="{name:'MealDetails', params: {id: meal.idMeal}}">
             <img :src="meal.strMealThumb" class="rounded-t-xl h-48 object-cover w-full"/>
         </router-link>
